b2130767fa6aa5172e2ff7ed0af14fb5ccfd19ab
Banditzero1 <ghnmdghjk@gmail.com>
2024-11-26 11:27:29 +0700

    leobit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Myname is Singharat Sanghiran");
+Console.WriteLine("My student id: 673450202-3");
+string is DataType--> ชนิดของข้อมูล 
+name is vis
